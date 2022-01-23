34e6f87b4a7705b65e4898cced4d5e095f31f452
luisangel <root.labm@gmail.com>
2022-01-23 12:43:50 -0500

    limpando elementos innecesarios

--- primera_semana/pre06.py
+++ primera_semana/pre06.py
@@ -3,28 +3,6 @@
 # Pedir los datos por consola(añadir cada persona a un alista , para evitar
 # que al sobreescribir los datos se pierdan)
 
-
-persona = {
-    "nombre": None,
-    "apellido": None,
-    "ciudad": None,
-    "lenguajes_programacion": []
-}
-
-persona["nombre"] = ["Luis", "Juan", "Gonzalo"]
-persona["apellido"] = "aaa", "bbb", "ccc"
-persona["ciudad"] = "Bogotá", "Buenos Aires", "Lima"
-persona["lenguajes_programacion"] = "Python", "Java", "Go"
-print(persona["nombre"])
-print(persona["nombre"][0])
-
-
-# pregunta06
-# De este arquitecto de persona,  crear por lo menos 3 personas
-# Pedir los datos por consola(añadir cada persona a un alista , para evitar
-# que al sobreescribir los datos se pierdan)
-
-
 persona = {
     "nombre": None,
     "apellido": None,
